7c56703349ef06a9e949eb672c8561c6dc00e2d5
SCS-CBU-CED-IAM <freddy.kaiser@swisscom.com>
2013-09-27 10:12:17 +0200

    Test script refs #20

---
+++ bash/mobileid-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# mobileid-tests.sh - 1.8
+#
+# Generic test script against Swisscom Mobile ID service
+# Dependencies: mobileid-sign.sh
+#
+# Change Log:
+#  1.0 27.09.2013: Initial version
+
+# Test specific error cases
+$PWD/mobileid-sign.sh -v +41000092101 "ILLEGAL_MSISDN" en
+$PWD/mobileid-sign.sh -v +41000092102 "MISSING_PARAM" en
+$PWD/mobileid-sign.sh -v +41000092103 "WRONG_DATA_LENGTH" en
+$PWD/mobileid-sign.sh -v +41000092104 "UNAUTHORIZED_ACCESS" en
+$PWD/mobileid-sign.sh -v +41000092105 "UNKNOWN_CLIENT" en
+$PWD/mobileid-sign.sh -v +41000092107 "INAPPROPRIATE_DATA" en
+$PWD/mobileid-sign.sh -v +41000092108 "INCOMPATIBLE_INTERFACE" en
+$PWD/mobileid-sign.sh -v +41000092109 "UNSUPPORTED_PROFILE" en
+$PWD/mobileid-sign.sh -v +41000092208 "EXPIRED_TRANSACTION" en
+$PWD/mobileid-sign.sh -v +41000092209 "OTA_ERROR" en
+$PWD/mobileid-sign.sh -v +41000020901 "BAD_OTA_RESPONSE_FORMAT" en
+$PWD/mobileid-sign.sh -v +41000092401 "USER_CANCEL" en
+$PWD/mobileid-sign.sh -v +41000092402 "PIN_NR_BLOCKED" en
+$PWD/mobileid-sign.sh -v +41000092403 "CARD_BLOCKED" en
+$PWD/mobileid-sign.sh -v +41000092404 "NO_KEY_FOUND" en
+$PWD/mobileid-sign.sh -v +41000092406 "PB_SIGNATURE_PROCESS" en
+$PWD/mobileid-sign.sh -v +41000092422 "NO_CERT_FOUND" en
+$PWD/mobileid-sign.sh -v +41000092900 "INTERNAL_ERROR" en
+
+# Test Heartbeat
+$PWD/mobileid-sign.sh -v +41000000000 "HEARTBEAT" en
