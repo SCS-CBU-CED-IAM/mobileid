57ff6205159cdf1f6d7f490b0e8e927284c406e0
H-P Waldegger <hans-peter.waldegger@swisscom.com>
2013-12-27 12:00:06 +0100

    Calls powershell script with given args, #50
    Replaced harcoded values with arguments for powershell script.

--- powershell/mobileid_sharp.bat
+++ powershell/mobileid_sharp.bat
@@ -1,2 +1,6 @@
-powershell .\mobileid_sharp.ps1 -Verbose 1 -PhoneNumber +41792080401 -Message "{'Do you want to login to corporate VPN?'}" -Language en
-pause
+@ECHO OFF
+set OLDDIR=%CD%
+chdir /d %~d0%
+chdir %~p0%
+powershell .\mobileid_sharp.ps1 %1 %2 %3 %4 %5
+chdir /d %OLDDIR% &rem restore current directory
