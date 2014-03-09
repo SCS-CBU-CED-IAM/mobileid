5d9bb9b00d742cbf20ef0b383952ece89bbce808
phaupt <philipp.haupt@swisscom.com>
2014-03-09 15:07:49 +0100

    Minor fixes

--- java/SAAJ/src/com/swisscom/mid/client/MobileidSign.java
+++ java/SAAJ/src/com/swisscom/mid/client/MobileidSign.java
@@ -251,7 +251,7 @@ public class MobileidSign {
 				} else if (innerResultList.item(l).getNodeName().equalsIgnoreCase("soapenv:Reason")) {
 					System.out.println("Fault Reason  : " + innerResultList.item(l).getChildNodes().item(0).getTextContent());
 				} else if (innerResultList.item(l).getNodeName().equalsIgnoreCase("soapenv:Detail")) {
-					System.out.println("Fault Detail  : " + innerResultList.item(l).getChildNodes().item(1).getTextContent());
+					System.out.println("Fault Detail  : " + innerResultList.item(l).getLastChild().getTextContent());
 				}
 			}
 		}
